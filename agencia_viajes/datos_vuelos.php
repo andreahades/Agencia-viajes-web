ba7709e84ad6c9ffcec7f37290d5b10a1e5f1377
andreahades <andrea.rivera.gonzalez@gmail.com>
2026-03-09 16:11:35 -0300

    Create datos_vuelos.php
    Archivo con datos de vuelos disponibles

---
+++ agencia_viajes/datos_vuelos.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+$VUELOS = [
+    1 => [
+        'origen' => 'Santiago',
+        'destino' => 'Lima',
+        'fecha' => '2026-03-15',
+        'precio' => 120000
+    ],
+    2 => [
+        'origen' => 'Santiago',
+        'destino' => 'Buenos Aires',
+        'fecha' => '2026-03-20',
+        'precio' => 145000
+    ],
+    3 => [
+        'origen' => 'Santiago',
+        'destino' => 'Rio de Janeiro',
+        'fecha' => '2026-03-25',
+        'precio' => 180000
+    ],
+    4 => [
+        'origen' => 'Concepción',
+        'destino' => 'Lima',
+        'fecha' => '2026-03-15',
+        'precio' => 135000
+    ],
+    5 => [
+        'origen' => 'Antofagasta',
+        'destino' => 'Buenos Aires',
+        'fecha' => '2026-03-20',
+        'precio' => 160000
+    ]
+];
